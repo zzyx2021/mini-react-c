f115624eb9427961fb7dd6f1770a440fdaf0d083
zzyx2021 <2498857834@qq.com>
2024-03-24 20:15:20 +0800

    add fiber

--- core/React.js
+++ core/React.js
@@ -19,20 +19,79 @@ function createElement(type, props, ...children) {
 }
 
 function render(el, container) {
-  const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(el.type)
+  nextWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [el]
+    }
+  }
+}
 
-  Object.keys(el.props).forEach((key) => {
+let nextWorkOfUnit = null
+function workloop(deadline) {
+  let shouldYield = false
+  while(!shouldYield && nextWorkOfUnit) {
+    nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workloop)
+}
+
+function createDom(type) {
+ return type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type)
+}
+
+function updateProps(dom, props) {
+  Object.keys(props).forEach((key) => {
     if (key !== 'children') {
-      dom[key] = el.props[key]
+      dom[key] = props[key]
     }
   })
-
-  const children = el.props.children
-  children.forEach((child) => {
-    render(child, dom)
-  })
-  container.append(dom)
 }
+
+function initChildren(fiber) {
+  const children = fiber.props.children
+  let prevChild = null
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null
+    }
+    if(index === 0){
+      fiber.child = newFiber
+    } else {
+      prevChild.sibling = newFiber
+    }
+    prevChild = newFiber
+  })
+}
+function performWorkOfUnit(fiber) {
+  if(!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type))
+    fiber.parent.dom.append(dom)
+    updateProps(dom, fiber.props)
+  }
+
+  //3.转换链表 设置好指针
+  initChildren(fiber)
+
+  //4.返回下一个要执行的任务
+  if(fiber.child) {
+    return fiber.child
+  }
+
+  if(fiber.sibling) {
+    return fiber.sibling
+  }
+
+  return fiber.parent?.sibling
+}
+
+requestIdleCallback(workloop)
 
 const React = {
   render,
